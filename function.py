a88f867fa664d541c029049cdc9ff2ae221ac26e
Ibrohim20032 <153578834+Ibrohim20032@users.noreply.github.com>
2026-03-01 22:38:16 +0500

    Yangilandi

--- function.py
+++ function.py
@@ -32,3 +32,47 @@
 #             print(f"{son} {i} soniga qoldiqsiz bo'linmaydi .")
 # number=int(input("son kiriting : "))
 # generateqoldiq(number)
+
+
+ #moslashuvchan funcsiyalar
+
+# def dicts(*son):
+#     dictresult=1
+#     for i in son:
+#         dictresult=dictresult*i
+#     return dictresult
+
+# sonlar=[]
+
+# while True:
+#     son=input("Son kiriting (hisoblash uchun * ni bosing ): ")
+#     if son!="*":
+#         son=int(son)
+#         sonlar.append(son)
+#     else:
+#         break
+# print(dicts(*sonlar))
+
+#Studentinfo 
+
+def studentinfo(name,surname,**addinfo):
+    newinfo={}
+    newinfo['Ism']=name
+    newinfo['Familiya']=surname
+    for i in addinfo:
+        newinfo[i]=addinfo[i]
+    return newinfo
+
+name=input("Talabaning ismini kiriting : ")
+surname=input("Talabaning familiyasini kiriting : ")
+addinfo={}
+while True:
+    ishora=input("Yana malumot kiritishni istaysizmi (ha/yoq):" )
+    if ishora!='yoq':
+        key=input("Kalit kiriting :")
+        value=input("Qiymat kiriting : ")
+        addinfo[key]=value
+    else:
+        break
+
+print("Talaba haqida malumotlar : ",studentinfo(name,surname,**addinfo))
